c7e5f37bd5f5e620e85ab6665118aebb9b6e6e40
Phil Downey <phildowney@pd-work-mac.local>
2025-08-14 15:32:15 +1000

    added bmfm smited examples

--- mcp-template/src/mcp_server_infer/__init__.py
+++ mcp-template/src/mcp_server_infer/__init__.py
@@ -4,11 +4,15 @@ from . import server
 import asyncio
 
 
+# Users can defined what Protocol they wish to run
+# If the none is started it will run in stdio
+# if sse defined sse protocol will be used
+# shttp streamable http protocol will be used.
+
+
 def main():
     if len(sys.argv) > 1:
-        server_type = sys.argv[
-            1
-        ].lower()  # Convert to lowercase for consistent checking
+        server_type = sys.argv[1].lower()  # Convert to lowercase for consistent checking
     else:
         # Default to 'stdio' or 'default' if no argument is provided
         server_type = "stdio"
